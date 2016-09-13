94f28628256b47671f6e9ccc87efbd2bd7ac4c7c
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 02:14:37 +0530

    session6assignment3

---
+++ app6.js
@@ -0,0 +1,13 @@
+var sum =0;
+for(var i =1;i<1001;i++){
+    
+   sum = sum+i; 
+}
+console.log("Using For loop SUM ="+sum);
+
+
+while(i<1001){
+    
+    sum = sum+i;
+}
+console.log("Using While loop SUM ="+sum);
